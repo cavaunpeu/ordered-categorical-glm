45c1860d5278879c92b54ae4ab975b20f171e2e3
William Wolf <wwolf@shopkeep.com>
2017-03-14 14:59:20 +0000

    plot observed cumulative probabilities

--- analysis.R
+++ analysis.R
@@ -4,6 +4,10 @@ library(tidyr)
 library(magrittr)
 library(ggplot2)
 
+# simulate data
 N <- 50
 feedback <- rmultinom(n = N, size = 1, prob = c(.1, .2, .3, .3, .1)) %>% t %>% max.col %>% as.data.frame %>% set_colnames("obs")
 ggplot(feedback, aes(x=obs)) + geom_bar()
+
+# plot observed cumulative probabilities
+feedback %>% table %>% "/"(N) %>% cumsum %>% plot(type="b")
