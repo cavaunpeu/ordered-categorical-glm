7b5867a82a6dc3a3c817a13dfae4dd112a1d8466
William Wolf <wwolf@shopkeep.com>
2017-03-14 14:30:22 +0000

    generate multinomial data, draw histogram

---
+++ analysis.R
@@ -0,0 +1,9 @@
+# analysis
+
+library(tidyr)
+library(magrittr)
+library(ggplot2)
+
+N <- 50
+feedback <- rmultinom(n = N, size = 1, prob = c(.1, .2, .3, .3, .1)) %>% t %>% max.col %>% as.data.frame %>% set_colnames("obs")
+ggplot(feedback, aes(x=obs)) + geom_bar()
