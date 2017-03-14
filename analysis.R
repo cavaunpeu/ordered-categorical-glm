0ce115fa09ae740acaab241b00c77a065e9a0557
William Wolf <wwolf@shopkeep.com>
2017-03-14 16:02:58 +0000

    fit model, examine estimates

--- analysis.R
+++ analysis.R
@@ -1,13 +1,31 @@
 # analysis
 
+library(dplyr)
 library(tidyr)
 library(magrittr)
 library(ggplot2)
+library(rethinking)
 
 # simulate data
 N <- 50
-feedback <- rmultinom(n = N, size = 1, prob = c(.1, .2, .3, .3, .1)) %>% t %>% max.col %>% as.data.frame %>% set_colnames("obs")
+probabilities <- c(.1, .2, .3, .3, .1)
+feedback <- rmultinom(n=N, size=1, prob=probabilities) %>% t %>% max.col %>% as.data.frame %>% set_colnames("obs")
 ggplot(feedback, aes(x=obs)) + geom_bar()
 
 # plot observed cumulative probabilities
-feedback %>% table %>% "/"(N) %>% cumsum %>% plot(type="b")
+cumulative_proportions <- feedback %>% table %>% "/"(N) %>% cumsum %>% as.data.frame %>% set_colnames("prop") %>% mutate(outcome=1:length(probabilities))
+ggplot(cumulative_proportions, aes(x=outcome, y=prop)) + geom_point()
+
+# fit model
+model <- map2stan(
+  alist(
+    obs ~ dordlogit( phi, cutpoints ),
+    phi <- 0,
+    cutpoints ~ dnorm(0, 10)
+  ),
+  data=feedback, start=list(cutpoints=c(-2, -1, 1, 2)), chains=2
+)
+
+# examine estimates
+sigmoid <- function(z) 1 / (1 + exp(-z))
+sigmoid( coef(model) ) %>% plot
