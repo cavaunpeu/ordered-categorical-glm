fda2f5b6710037b37c50f10ae7911386d2754897
William Wolf <wwolf@shopkeep.com>
2017-03-15 10:08:40 +0000

    pad all equals signs with whitespace

--- analysis.R
+++ analysis.R
@@ -9,12 +9,12 @@ library(rethinking)
 # simulate data
 N <- 50
 probabilities <- c(.1, .2, .3, .3, .1)
-feedback <- rmultinom(n=N, size=1, prob=probabilities) %>% t %>% max.col %>% as.data.frame %>% set_colnames("obs")
-ggplot(feedback, aes(x=obs)) + geom_bar()
+feedback <- rmultinom(n = N, size = 1, prob = probabilities) %>% t %>% max.col %>% as.data.frame %>% set_colnames("obs")
+ggplot(feedback, aes(x = obs)) + geom_bar()
 
 # plot observed cumulative probabilities
-cumulative_proportions <- feedback %>% table %>% "/"(N) %>% cumsum %>% as.data.frame %>% set_colnames("observed") %>% mutate(outcome=1:length(probabilities))
-ggplot(cumulative_proportions, aes(x=outcome, y=observed)) + geom_point()
+cumulative_proportions <- feedback %>% table %>% "/"(N) %>% cumsum %>% as.data.frame %>% set_colnames("observed") %>% mutate(outcome = 1:length(probabilities))
+ggplot(cumulative_proportions, aes(x = outcome, y = observed)) + geom_point()
 
 # fit model
 model <- map2stan(
@@ -23,7 +23,7 @@ model <- map2stan(
     phi <- 0,
     cutpoints ~ dnorm(0, 10)
   ),
-  data=feedback, start=list(cutpoints=c(-2, -1, 1, 2)), chains=2
+  data = feedback, start = list(cutpoints = c(-2, -1, 1, 2)), chains = 2
 )
 
 # examine estimates
@@ -35,15 +35,15 @@ cutpoint_mu <- apply(X = cutpoint_samples, MARGIN = 2, FUN = mean)
 cutpoint_PI <- apply(X = cutpoint_samples, MARGIN = 2, FUN = quantile, c(.04, .96))
 
 data.frame(
-    mu = cutpoint_mu,
-    mu_lower_bound = cutpoint_PI[1,],
-    mu_upper_bound = cutpoint_PI[2,]
-  ) %>%
+  mu = cutpoint_mu,
+  mu_lower_bound = cutpoint_PI[1,],
+  mu_upper_bound = cutpoint_PI[2,]
+) %>%
   bind_cols(cumulative_proportions[1:nrow(cumulative_proportions)-1,]) %>%
-  ggplot(., aes(x=outcome)) +
-    geom_point(size=2, aes(y=observed, color="Observed")) +
-    geom_line(aes(y=mu, color="Estimated"), linetype="dashed") +
-    geom_ribbon(aes(ymax=mu_upper_bound, ymin=mu_lower_bound), alpha=.05, fill="red")
+  ggplot(., aes(x = outcome)) +
+  geom_point(size = 2, aes(y = observed, color = "Observed")) +
+  geom_line(aes(y = mu, color = "Estimated"), linetype = "dashed") +
+  geom_ribbon(aes(ymax = mu_upper_bound, ymin = mu_lower_bound), alpha = .05, fill = "red")
 
 # simulate new observations
 compute_empirical_multinomial_expected_value <- function(prob, size = 50) {
@@ -59,4 +59,4 @@ simulated_probabilities <- cbind(cutpoint_samples, 1) - cbind(0, cutpoint_sample
 
 # plot new observations
 apply(X = simulated_probabilities, MARGIN = 1, FUN = compute_empirical_multinomial_expected_value) %>%
-  qplot(bins=30, color=I("white"), fill=I("deepskyblue3"), alpha=I(.8))
+  qplot(bins = 30, color = I("white"), fill = I("deepskyblue3"), alpha = I(.8))
