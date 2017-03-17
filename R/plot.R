612fbd4006ad879559363124cbfae7873344ad57
William Wolf <wwolf@shopkeep.com>
2017-03-17 10:14:01 +0000

    refactor functions to extract cdf samples, and convert cdf samples to pmfs

--- R/plot.R
+++ R/plot.R
@@ -1,8 +1,18 @@
 # plot
 
 sigmoid <- function(z) 1 / (1 + exp(-z))
-extract_cutpoint_samples <- function(model) extract(model)$cutpoints %>% sigmoid
-convert_cdf_to_pmf <- function(cutpoint_samples) cbind(cutpoint_samples, 1) - cbind(0, cutpoint_samples)
+extract_cutpoint_samples <- function(model) extract(model)$cutpoints
+
+extract_cdf_samples <- function(model) {
+  model %>%
+    extract_cutpoint_samples %>%
+    sigmoid %>%
+    as.data.frame %>%
+    cbind(1) %>%
+    set_colnames(outcomes)
+}
+
+convert_cdf_to_pmf <- function(cdf_samples) cdf_samples - cbind(0, cdf_samples[ , -ncol(cdf_samples) ])
 
 compute_empirical_multinomial_expected_value <- function(prob, size = 500) {
   rmultinom(n = 1, size = size, prob = prob) %>%
@@ -12,16 +22,15 @@ compute_empirical_multinomial_expected_value <- function(prob, size = 500) {
 }
 
 generate_weighted_average_posterior_predictions <- function(pmf_samples, model_name) {
-  predictions <- apply(X = pmf_samples, MARGIN = 1, FUN = compute_empirical_multinomial_expected_value) %>%
+  apply(X = pmf_samples, MARGIN = 1, FUN = compute_empirical_multinomial_expected_value) %>%
     as.data.frame %>%
-    set_colnames("weighted_average")
-  predictions$model <- model_name
-  return( predictions )
+    set_colnames("weighted_average") %>%
+    cbind(model = model_name)
 }
 
 compute_posterior_weighted_average_density <- function(model, model_name) {
   model %>%
-    extract_cutpoint_samples %>%
+    extract_cdf_samples %>%
     convert_cdf_to_pmf %>%
     generate_weighted_average_posterior_predictions(model_name)
 }
@@ -42,8 +51,8 @@ generate_comparative_density_plot <- function(first_model, second_model, first_m
 }
 
 generate_comparative_cdf_distribution_plot <- function(first_model, second_model, outcomes, first_model_name = "A", second_model_name = "B") {
-  cdf_samples_first <- first_model %>% extract_cutpoint_samples %>% as.data.frame %>% cbind(1) %>% set_colnames(outcomes) %>% cbind(model = first_model_name)
-  cdf_samples_second <- second_model %>% extract_cutpoint_samples %>% as.data.frame %>% cbind(1) %>% set_colnames(outcomes) %>% cbind(model = second_model_name)
+  cdf_samples_first <- first_model %>% extract_cdf_samples %>% cbind(model = first_model_name)
+  cdf_samples_second <- second_model %>% extract_cdf_samples %>% cbind(model = second_model_name)
   rbind(cdf_samples_first, cdf_samples_second) %>%
     mutate(trial = 1:nrow(.)) %>%
     melt(id.vars=c("model", "trial")) %>%
@@ -53,7 +62,7 @@ generate_comparative_cdf_distribution_plot <- function(first_model, second_model
     theme_minimal() +
     labs(
       title = "Posterior CDF Distribution",
-      subtitle = "(If one is distinctly more \"bent\" towards the bottom-right than the other, it's probably better)",
+      subtitle = "(If one is distinctly more \"bent towards the bottom-right\" than the other, it's probably better)",
       x = "Score",
       y = "Cumulative Probability"
     )
