f4be5745e37085ab664c0e146c3fb0a79fb6d16c
William Wolf <wwolf@shopkeep.com>
2017-03-17 09:41:49 +0000

    add function for generative comparative cdf distribution plot

--- R/plot.R
+++ R/plot.R
@@ -35,8 +35,26 @@ generate_comparative_density_plot <- function(first_model, second_model, first_m
     guides(fill = guide_legend(title="Population")) +
     labs(
       title = "Posterior Predictive Density Plot of Weighted Average Scores",
-      subtitle = "(If one is distinctly to the right of the other, then it's probably better)",
+      subtitle = "(If one is distinctly to the right of the other, it's probably better)",
       x = "Weighted Average",
       y = "Count"
+    )
+}
+
+generate_comparative_cdf_distribution_plot <- function(first_model, second_model, outcomes, first_model_name = "A", second_model_name = "B") {
+  cdf_samples_first <- first_model %>% extract_cutpoint_samples %>% as.data.frame %>% cbind(1) %>% set_colnames(outcomes) %>% cbind(model = first_model_name)
+  cdf_samples_second <- second_model %>% extract_cutpoint_samples %>% as.data.frame %>% cbind(1) %>% set_colnames(outcomes) %>% cbind(model = second_model_name)
+  rbind(cdf_samples_first, cdf_samples_second) %>%
+    mutate(trial = 1:nrow(.)) %>%
+    melt(id.vars=c("model", "trial")) %>%
+    ggplot(aes(x = variable, y = value, group = factor(trial))) +
+    geom_line(aes(color = model), alpha = .05) +
+    guides(colour = guide_legend(override.aes = list(alpha = 1), title = "Population")) +
+    theme_minimal() +
+    labs(
+      title = "Posterior CDF Distribution",
+      subtitle = "(If one is distinctly more \"bent\" towards the bottom-right than the other, it's probably better)",
+      x = "Score",
+      y = "Cumulative Probability"
     )
 }
